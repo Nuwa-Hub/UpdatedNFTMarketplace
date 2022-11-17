6e6fe1236d5e0adab16da272356e3645efd153f1
Shilpa-Perera <laksahaniperera@gmail.com>
2022-11-17 22:09:10 +0530

    filters

--- components/collection/NFTCard.jsx
+++ components/collection/NFTCard.jsx
@@ -25,23 +25,26 @@ export default function NFTCard() {
 	//get all NFTs that include to the relevent collection
 	const nfts = useSelector((state) => state.NFT.NFTs);
 	console.log("nfts", nfts);
+	console.log("filtered", filterData);
 
 	const handleFilter = (fileter_id , nfts) => {
+		setFilter(true);
 		if (fileter_id == 1) {
 			let cpy = [...nfts];
-			let sorted = cpy.sort(
-				(p1, p2) => (p1.price < p2.price) ? 1 : (p1.price > p2.price) ? -1 : 0);
+			let sorted = cpy.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
 			setFilterData(sorted);
 			setLowToHigh(true);
-			
+			setHighToLow(false);
+			setMostFavorite(false);
 
 			//get all NFTs that include to the relevent collection
 		} else if (fileter_id == 2) {
 			let cpy = [...nfts];
-			let sorted = cpy.sort(
-				(p1, p2) => (p1.price > p2.price) ? 1 : (p1.price < p2.price) ? -1 : 0);
+			let sorted = cpy.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
 			setFilterData(sorted);
 			setHighToLow(true);
+			setLowToHigh(false);
+			setMostFavorite(false);
 			//get all NFTs that include to the relevent collection
 		} else if (fileter_id == 3) {
 			setMostFavorite(true);
@@ -69,7 +72,8 @@ export default function NFTCard() {
 						)}
 						{lowToHigh && (
 							<button
-								onClick={() => setLowToHigh(false)}
+								onClick={() => {setLowToHigh(false)
+								setFilter(false)}}
 								class="p-1 px-2 m-0.5 text-sm font-bold border-2 text-blue-800 border-blue-500 bg-white rounded-lg"
 							>
 								<span class="w-full inline-flex leading-4 align-middle">
@@ -96,7 +100,8 @@ export default function NFTCard() {
 						)}
 						{highToLow && (
 							<button
-								onClick={() => setHighToLow(false)}
+								onClick={() => {setHighToLow(false)
+									setFilter(false)}}
 								class="p-1 px-2 m-0.5 text-sm font-bold border-2 text-blue-800 border-blue-500 bg-white rounded-lg"
 							>
 								<span class="w-full inline-flex leading-4 align-middle">
@@ -143,7 +148,44 @@ export default function NFTCard() {
 			</div>
 			<div className="container px-2 py-2 mx-auto lg:pt-12 lg:px-2">
 				<div className="flex flex-wrap -m-1 md:-m-2">
-					{nfts.map((nft) => (
+					{filter && filterData.map((nft) => (
+						<div
+							key={nft._id}
+							className="flex flex-wrap w-full  sm:w-1/2 md:w-1/3 lg:w-1/4 "
+						>
+							<div className="group max-w-sm  m-4 bg-zinc-200 rounded-lg border border-gray-200  ">
+								<Link href={`/nft/${nft._id}`}>
+									<div className="w-full aspect-square  overflow-hidden">
+										<img
+											alt="gallery"
+											className="block object-cover object-center w-full h-full rounded-lg hover:shadow-lg transition ease-in-out  hover:-translate-y-1 hover:scale-110"
+											src={nft.Img}
+										/>
+									</div>
+								</Link>
+
+								<div className="px-5 pt-2">
+									<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+										NFT Name
+									</h5>
+
+									<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+										price
+									</p>
+									<p className="mb-3 font-normal text-gray-700 dark:text-gray-400 group-hover:hidden">
+										Last Sale
+									</p>
+								</div>
+								<button
+									type="button"
+									className="hidden flex h-10 bg-gradient-to-r from-indigo-800 via-green-500 to-teal-200  w-full rounded-none group-hover:block"
+								>
+									Buy Now
+								</button>
+							</div>
+						</div>
+					))}
+					{! filter && nfts.map((nft) => (
 						<div
 							key={nft._id}
 							className="flex flex-wrap w-full  sm:w-1/2 md:w-1/3 lg:w-1/4 "
