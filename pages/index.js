9272cffa2a57b479dc59f47eb210e3eea729985a
Nuwa-Hub <you@example.com>
2022-11-20 20:47:01 +0530

    add

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,6 @@ export default function Home() {
       <GifCollection />
        {/* <NFTVedio/>   */}
       <RaffleHelper />
-
       {/* <MarketVedio/>  */}
       <ProductLists />
       <Trending />
