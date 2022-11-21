51210b9b42e7e444da8cebae6138518ed909514a
KalinduGandara <kalindugandara16@gmail.com>
2022-11-21 15:42:04 +0530

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'ff9v4k',
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://updated-nft-marketplace.vercel.app/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
